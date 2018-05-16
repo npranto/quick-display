b767e876fdac86a6c93248a4237d130cd8c8456c
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-05-16 16:00:53 -0400

    adds layout for features component

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -2,9 +2,48 @@ import './Features.css';
 
 const Features = () => {
   return `
-		<div class="Features">
-			Features
-		</div>
+    <div class="Features">
+      <div class="Feature">
+        <div class="feature-description">
+          <p class="description">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a color.
+          </p>
+        </div>
+        <div class="feature-photo">
+          <img src="https://images.unsplash.com/photo-1502201563651-826cbb30c3dd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f0cf47ff5403937c28bd4f856bf7d66e&auto=format&fit=crop&w=800&q=80" alt="Feature Photo" />
+        </div>
+      </div>
+      <div class="Feature">
+        <div class="feature-description">
+          <p class="description">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a color.
+          </p>
+        </div>
+        <div class="feature-photo">
+          <img src="https://images.unsplash.com/photo-1509576931792-214960705f8a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7c3b1b3b6dc3f261de3a723417251e23&auto=format&fit=crop&w=966&q=80" alt="Feature Photo" />
+        </div>
+      </div>
+      <div class="Feature">
+        <div class="feature-description">
+          <p class="description">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a color.
+          </p>
+        </div>
+        <div class="feature-photo">
+          <img src="https://images.unsplash.com/photo-1494366222322-387658a1a976?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=24100980ab1a60783e1c847b7965bd11&auto=format&fit=crop&w=1051&q=80" alt="Feature Photo" />
+        </div>
+      </div>
+      <div class="Feature">
+        <div class="feature-description">
+          <p class="description">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a color.
+          </p>
+        </div>
+        <div class="feature-photo">
+          <img src="https://images.unsplash.com/photo-1516235328748-f58ef03d0aad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=332d1ced1c5cb80105268fb90988dcb7&auto=format&fit=crop&w=634&q=80" alt="Feature Photo" />
+        </div>
+      </div>
+    </div>
   `;
 };
 
